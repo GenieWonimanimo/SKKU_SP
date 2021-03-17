a336c67fa60e32e978d82882b1f978b44b2d7685
jiweon <kor98won@gmail.com>
2021-03-17 23:27:25 +0900

    HW1

---
+++ SP_HW1_2021f/sfp.c
@@ -0,0 +1,28 @@
+#include "sfp.h"
+#include <stdlib.h>
+sfp int2sfp(int input){
+	
+}
+
+int sfp2int(sfp input){
+}
+
+sfp float2sfp(float input){
+}
+
+float sfp2float(sfp input){
+}
+
+sfp sfp_add(sfp a, sfp b){
+	
+}
+
+sfp sfp_mul(sfp a, sfp b){
+	
+}
+
+char* sfp2bits(sfp result){
+
+}
+
+
