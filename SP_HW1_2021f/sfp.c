71b083549ceb5110e4270c1c13cc6e2e16cfbfe2
kor98won_linux <kor98won@gmail.com>
2021-03-19 23:54:36 -0700

    Pow_v3

--- SP_HW1_2021f/sfp.c
+++ SP_HW1_2021f/sfp.c
@@ -13,10 +13,6 @@
 double Pow(double a, int b) {
 	if (b == 0)
 		return 1;
-    if (b == 1)
-        return a;
-    if (b == -1)
-        return 1 / a;
     double res = Pow(a, b / 2);
     if (b % 2 == 0)
         return res * res;
